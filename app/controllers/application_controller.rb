8f2310a4880939a711b5aa97bc324d152f290eb8
bognerth <bognerth@gmail.com>
2014-02-18 21:26:20 +0100

    Wiki

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :current_user
+  before_filter :current_user, :current_lecture
 
   helper_method :current_user
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
     end   
+  end
+
+  helper_method :current_lecture
+  def current_lecture
+    if params[:lecture]
+      session[:wiki_lecture] = params[:lecture]
+      @lecture = params[:lecture]
+    elsif session[:wiki_lecture]
+      @current_lecture = session[:wiki_lecture]
+    else
+      redirect_to ENV['ROOT_DOMAIN'], alert: "Der Kurs ist nicht angegeben." 
+    end
   end
 
   def authorize
